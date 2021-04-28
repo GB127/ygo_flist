e7fa901384f849aeb88838b13e9d78874d696b3a
Guylain <guylain.breton.1@ulaval.ca>
2021-04-27 22:47:13 -0400

    update script : one comment, some modifications to auto_ban

--- script.py
+++ script.py
@@ -1,7 +1,10 @@
 import requests
 
 
-def ban_later():
+def auto_ban():
+    """Function that will generate the list of banned IDs.
+        returns a str ready to write in the file.
+    """
     end = "10/26/2010"
     today = "04/27/2021"
     rep_toban = requests.get(f"https://db.ygoprodeck.com/api/v7/cardinfo.php?&startdate={end}&enddate={today}&dateregion=tcg_date")
@@ -9,7 +12,7 @@ def ban_later():
         all_toban = rep_toban.json()["data"]
     else:
         raise BaseException("Something went wrong")
-    toreturn = ""
+    toreturn = "#[2005.4 GOAT]\n!2010.10 TG5\n#Cards after TG5\n"
     for card in all_toban:
         toreturn += f"{card['id']} 0 \n"
     return toreturn
@@ -25,8 +28,6 @@ def get_cards_pool():
     return card_pool
 
 if __name__ == "__main__":
-    towrite = ""
-    towrite += ban_later()
-
+    pass
 
 
