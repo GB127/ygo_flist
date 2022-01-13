f00ccb8a0af456c10fa2d7cf3645fcd8f206b07a
Guylain <guylain.breton.1@ulaval.ca>
2022-01-13 17:11:04 -0500

    Forbidden list in class

--- draft.py
+++ draft.py
@@ -16,6 +16,143 @@ class yugioh_modes:
     players = ["Guylain", "Maxime"]
     all_cards = []
     accepted_cards = []
+    banlist = {
+        0: [
+            "Black Luster Soldier - Envoy of the Beginning",
+            "Chaos Emperor Dragon - Envoy of the End",
+            "Cyber Jar",
+            "Cyber-Stein",
+            "Dark Magician of Chaos",
+            "Dark Strike Fighter",
+            "Destiny HERO - Disk Commander",
+            "Fiber Jar",
+            "Magical Scientist",
+            "Magician of Faith",
+            "Makyura the Destructor",
+            "Sinister Serpent",
+            "Thousand-Eyes Restrict",
+            "Tribe-Infecting Virus",
+            "Tsukuyomi",
+            "Victory Dragon",
+            "Witch of the Black Forest",
+            "Yata-Garasu",
+            "Butterfly Dagger - Elma",
+            "Card of Safe Return",
+            "Change of Heart",
+            "Confiscation",
+            "Dark Hole",
+            "Delinquent Duo",
+            "Dimension Fusion",
+            "Graceful Charity",
+            "Harpie's Feather Duster",
+            "Last Will",
+            "Monster Reborn",
+            "Metamorphosis",
+            "Mirage of Nightmare",
+            "Painful Choice",
+            "Pot of Greed",
+            "Premature Burial",
+            "Raigeki",
+            "Snatch Steal",
+            "The Forceful Sentry",
+            "Crush Card Virus",
+            "Exchange of the Spirit",
+            "Imperial Order",
+            "Last Turn",
+            "Ring of Destruction",
+            "Time Seal"],
+        1:[
+            'Advanced Ritual Art', 
+            'Allure of Darkness', 
+            'Black Rose Dragon', 
+            'Blackwing - Gale the Whirlwind', 
+            'Brain Control', 
+            'Brionac, Dragon of the Ice Barrier', 
+            'Burial from a Different Dimension', 
+            'Call of the Haunted', 
+            'Card Destruction',
+            'Card Trooper', 
+            'Ceasefire', 
+            'Chaos Sorcerer',
+            'Charge of the Light Brigade',
+            'Cold Wave',
+            'Dark Armed Dragon',
+            'Destiny Draw',
+            'Elemental HERO Stratos',
+            'Emergency Teleport',
+            'Exodia the Forbidden One',
+            'Foolish Burial',
+            'Future Fusion',
+            'Giant Trunade',
+            'Gladiator Beast Bestiari',
+            'Gorz the Emissary of Darkness',
+            'Goyo Guardian',
+            'Gravity Bind',
+            'Heavy Storm',
+            'Left Arm of the Forbidden One',
+            'Left Leg of the Forbidden One',
+            'Level Limit - Area B',
+            'Limiter Removal',
+            'Lumina, Lightsworn Summoner',
+            'Magic Cylinder',
+            'Magical Explosion',
+            'Marshmallon',
+            'Megamorph',
+            'Mezuki',
+            'Mind Control',
+            'Mind Crush',
+            'Mind Master',
+            'Mirror Force',
+            'Monster Gate',
+            'Morphing Jar',
+            'Mystical Space Typhoon',
+            'Necro Gardna',
+            'Necroface',
+            'Neo-Spacian Grand Mole',
+            'Night Assailant',
+            'Ojama Trio', 
+            'One for One',
+            'Overload Fusion', 
+            'Plaguespreader Zombie',
+            'Reasoning', 
+            'Reinforcement of the Army',
+            'Rescue Cat',
+            'Return from the Different Dimension', 
+            'Right Arm of the Forbidden One', 
+            'Right Leg of the Forbidden One',
+            'Sangan',
+            'Scapegoat',
+            'Snipe Hunter',
+            'Solemn Judgment',
+            'Spirit Reaper',
+            'Summoner Monk',
+            'Swords of Revealing Light',
+            'The Transmigration Prophecy',
+            'Torrential Tribute',
+            'Tragoedia',
+            'Trap Dustshoot',
+            'Wall of Revealing Light'],
+        2:[
+            "Cyber Dragon",
+            "Dandylion",
+            "Demise, King of Armageddon",
+            "Destiny HERO - Malicious",
+            "Goblin Zombie",
+            "Honest",
+            "Judgment Dragon",
+            "Lonefire Blossom",
+            "Treeborn Frog",
+            "Black Whirlwind",
+            "Chain Strike",
+            "Gold Sarcophagus",
+            "Magical Stone Excavation",
+            "United We Stand",
+            "Bottomless Trap Hole",
+            "Royal Decree",
+            "Royal Oppression",
+            "Skill Drain",
+            "Ultimate Offering"]}
+
     with open("cards_list.txt", "r") as file:
         tempo = file.read().rstrip("\n").split("\n")
         for carte in requests.get(f"https://db.ygoprodeck.com/api/v7/cardinfo.php").json()["data"]:
@@ -42,7 +179,7 @@ class yugioh_modes:
             toreturn += f"{' ' * width}{string2[start:]}"
             return toreturn
 
-        string = f'{start}'
+        string = f'{start} - {len(self[attribute])} cards'
         if not self[attribute]:
             return string
 
@@ -121,8 +258,12 @@ class Draft_manager(yugioh_modes):
             self.pool.pop(0)
 
     def __str__(self):
-        string = super().__str__()
-        return string
+        line = "\n" + "-"*73 + "\n"
+        strings = super().__str__().split(line)
+        strings.insert(1, self.str_cards("Cards pool", "todraft"))
+
+        return line.join(strings)
 
 test = Draft_manager(300, "Guylain")
-test.save()
+test(1)
+print(test)
