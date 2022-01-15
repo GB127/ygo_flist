a7d72b2fff7e595c8900ab5678deb37d0fb91299
Guylain <guylain.breton.1@ulaval.ca>
2022-01-15 12:10:10 -0500

    Compartimentalisons le call de draft

--- draft.py
+++ draft.py
@@ -16,26 +16,34 @@ class Draft(yugioh_modes):
                         shuffle = Shuffle between selections?
                     }
             """
-        self.filters = draft_filters
-        if params["themed_draft"]:
-            for _ in range(params["todeal"] - len(self.filters)):
-                scape = "id"
-                while scape in ["id", "card_prices","desc", "name", "card_images", "card_sets"]:
-                    scape = choice(list(choice(self.pool).keys()))
-                valeur = choice(list(set([x[scape] for x in self.pool if scape in x])))
-                self.filters.append((scape, valeur))
+        def build_filters_spots():
+            def complete_themed_draft():
+                for _ in range(params["todeal"] - len(self.filters)):
+                    scape = "id"
+                    while scape in ["id", "card_prices","desc", "name", "card_images", "card_sets"]:
+                        scape = choice(list(choice(self.pool).keys()))
+                    valeur = choice(list(set([x[scape] for x in self.pool if scape in x])))
+                    self.filters.append((scape, valeur))
 
-        self.todraft = [None for _ in range(params["todeal"])]
+            self.filters = draft_filters
+            if params["themed_draft"]:
+                complete_themed_draft()
+            self.todraft = [None for _ in range(params["todeal"])]
+            self.deal(params["shuffle"])
+
+        def game_loop():
+            while any([len(self[x]) < params["max_draft"] for x in self.players]):
+                for _ in range(params["select"]):
+                    self.select()
+                    if params["fill"]: self.deal(params["shuffle"])
+                if not params["fill"]: self.deal(params["shuffle"])
+                self.player_turn = abs(self.player_turn) - 1
+
         if (-1 + params["todeal"] + params["max_draft"] *2) > len(self.pool):
             raise ygo_Error(f'You have not enough cards in the pool to play with these parameters.\nYou need {-1 + params["todeal"] + params["max_draft"] *2} cards in pool. The pool has {len(self.pool)}')
-        self.deal(params["shuffle"])
-        while any([len(self[x]) < params["max_draft"] for x in self.players]):
-            for _ in range(params["select"]):
-                self.select()
-                if params["fill"]: self.deal(params["shuffle"])
-            if not params["fill"]: self.deal(params["shuffle"])
-            self.player_turn = abs(self.player_turn) - 1
-        print(self)
+
+        build_filters_spots()
+        game_loop()
         self.save(seed)
     
     @verify_int
@@ -61,14 +69,14 @@ class Draft(yugioh_modes):
             self.discarded += [x for x in self.todraft if x]
             self.todraft = [None for _ in range(len(self.todraft))]
         for spot, filtre in enumerate(self.filters):
-            if self.todraft[spot]: continue
-            if not self.pool:
+            if self.todraft[spot]: continue  # Spot is not empty
+            if not self.pool:  # Check if pool is empty!
                 self.pool, self.discarded = self.discarded, []
             try:
                 id = get_card(filtre[0], filtre[1]) if get_card(filtre[0], filtre[1]) else 0
                 self.todraft[spot] = self.pool[id]
                 self.pool.pop(id)
-            except TypeError:
+            except TypeError:  # Use any card. (No filter)
                 self.todraft[spot] = self.pool[0]
                 self.pool.pop(0)
 
