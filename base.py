33c59d382099c19f1f04614d531a6670f0e0a09b
Guylain <guylain.breton.1@ulaval.ca>
2022-01-16 00:32:11 -0500

    add checksum step inpu

--- base.py
+++ base.py
@@ -227,7 +227,9 @@ class yugioh_modes:
         print(f'Seed : {self.seed}')
         for _ in range(5):
             print(f'     {choice(self.accepted_cards)["name"]}')
-        input("Do the checksum match?\nIf yes, continue\nElse, close the terminal and restart.")
+        if not self.debug:
+            input("Do the checksum match?\nIf yes, continue\nElse, close the terminal and restart.")
+    
     def save(self):
         towrite = f"#[2005.4 GOAT]\n!TEST\n#Cards after TG5\n"
         with open(f'cards_alt.txt') as file:
