9903d13e3e3bca2f4dc49bc75795e0d51d898c29
Guylain <guylain.breton.1@ulaval.ca>
2022-01-15 20:12:00 -0500

    Fix the main.py

--- main.py
+++ main.py
@@ -1,44 +1,38 @@
-from cards import *
+from draft import Draft
 import argparse
-import random
+from random import random
 
 def getcommand():
     parser = argparse.ArgumentParser(description="Yugioh Card pooler")
-    parser.add_argument("seed", 
-                        action="store",
-                        help="Seed for the randomization",
-                        default=str(random.random())[2:],
-                        metavar="",
-                        type=str)    
+    parser.add_argument("--pool", action="store", type=int, default=200)
+    parser.add_argument("--seed",
+                            action="store",
+                            help="Seed for the randomization",
+                            default=str(random())[2:],
+                            metavar="seed",
+                            type=str)
+    parser.add_argument("-d", "--draft",
+                            nargs=3,
+                            help="Draft mode : [Total, River, select]")
+    parser.add_argument("-s", "--shuffle",
+                            action="store_true",
+                            help="Shuffle cards between actions")
+    parser.add_argument("-f", "--fill",
+                            action="store_true",
+                            help="Fill cards between actions")
+    parser.add_argument("-t", "--themed",
+                            action="store_true",
+                            help="Cards spots follows a theme.")
+
+
     options = parser.parse_args()
     return options
 
 if __name__ == "__main__":
     options = getcommand()
-    if options.seed == "0":
-        filename = "GB_MAX_BANLIST_final.lflist.conf"
-    else:
-        random.seed(options.seed)
-        filename = f'Random_pool_{options.seed}.lflist.conf'
-    auto_ban(filename)
-    all_cards = get_cards_pool()
-    if options.seed != "0":
-        monsters, spells, traps, extras = split_pool(all_cards)
-        for ty in [monsters, spells, traps, extras]: random.shuffle(ty)
-
-        # Monsters:
-        allow_pool(filename, monsters[:200])
-        ban_pool(filename, monsters[201:])
-
-        # Spells:
-        allow_pool(filename, spells[:100])
-        ban_pool(filename, spells[101:])
-        # Traps:
-        allow_pool(filename, traps[:100])
-        ban_pool(filename, traps[101:])
-
-        # Extras:
-        allow_pool(filename, extras[:50])
-        ban_pool(filename, extras[51:])
-    else:
-        allow_pool(filename, get_cards_pool())
+    if options.draft:
+        game = Draft(300,seed_str=options.seed, player="Maxime", debug=True)
+        game(options.draft, 
+                options.fill,
+                options.shuffle,
+                options.themed)
